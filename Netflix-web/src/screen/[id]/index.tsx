3138ba431937d4b3dae0db5a0714fb31af190355
Jeffrey Echano <90635364+Jefuriii83@users.noreply.github.com>
2024-03-20 13:37:51 +0800

    Delete Netflix-web directory

--- Netflix-web/src/screen/[id]/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useLocalSearchParams } from "expo-router";
-import { Text } from "react-native";
-
-export default function Page(){
-    const { id } = useLocalSearchParams();
-
-    return <Text>ID: {id}</Text>
-}
